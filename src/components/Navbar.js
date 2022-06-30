cd41e52bf02c4c5dfbad8ee0ba4d6aee051dabab
shams42 <a7medbasher811@gmail.com>
2022-06-30 18:20:28 +0200

    adding Skills

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "../styles/Navbar.css"
-import ReorderIcon from "@material-ui/icons/Reorder";
-
+import ReorderIcon from '@material-ui/icons/Reorder';
 function Navbar() {
   const [expandNavbar, setExpandNavbar] = useState(false);
 

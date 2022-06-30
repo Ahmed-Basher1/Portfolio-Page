9958091bd164cdf8b5f47dcaddc53c70079c61ab
shams42 <a7medbasher811@gmail.com>
2022-06-30 23:03:50 +0200

    adding projects

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,9 +1,62 @@
 import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@material-ui/icons/School";
+import WorkIcon from "@material-ui/icons/Work";
 
-function Experience(){
-    return(
-        <div>Experience</div>
-    )
+function Experience() {
+  return (
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2013 - 2016"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+          Mansoura secondary school 
+          </h3>
+          <p> High School - Thanaweya Amma </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2016 - 2020"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+          Faculty of Computers and information science at Mansoura Universtity
+          </h3>
+
+          <h4 className="vertical-timeline-element-subtitle">
+            Bachelor's Degree
+          </h4>
+
+          <p> Computer Science</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="Apr - Jul /2022"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Mearn stack Developer
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+           Information Technology insitute, Mansoura
+          </h4>
+          <p>Mearn stack Track .</p>
+        </VerticalTimelineElement>
+
+      
+      </VerticalTimeline>
+    </div>
+  );
 }
 
-export default Experience
+export default Experience;
